b0a60c05b50157f513f3ee72b9633d0af2317a3c
ymcagodme <ymcagodme@gmail.com>
2014-12-04 22:44:55 -0800

    init project files

---
+++ final_proj/final_proj.ino
@@ -0,0 +1,111 @@
+#define trigPin 12
+#define echoPin 13
+#define leftMotor 14
+#define rightMotor 2
+#define leftPhoto 4
+#define rightPhoto 6
+
+int g = GREEN_LED;
+int r = RED_LED;
+int t = 200;
+int turnDelay = 3000;
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(g, OUTPUT);
+  pinMode(r, OUTPUT);
+  
+  pinMode(leftMotor, OUTPUT);
+  pinMode(rightMotor, OUTPUT);
+  
+//  pinMode(leftPhoto, INPUT);
+//  pinMode(rightPhoto, INPUT);
+  
+  turnOffMotor();
+}
+
+void loop() {
+//  int leftPhotoVal = digitalRead(leftPhoto);
+//  int rightPhotoVal = digitalRead(rightPhoto);
+//  Serial.println("left value");
+//  Serial.println(leftPhotoVal);
+//  Serial.println("right value");
+//  Serial.println(rightPhotoVal);  
+  
+  
+  // int freq;
+  float distance = calDistance();
+
+
+  if (distance >= 80) {
+    setGOn();
+    setROff();
+    turnOnMotor();
+  }
+  else {
+    setROn();
+    setGOff();
+    doTurn();
+    delay(turnDelay);
+  }
+  
+
+  
+  delay(100);
+  
+}
+
+float calDistance() {
+  int duration;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(12);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(100);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  float distance = duration / 29 / 2;
+  Serial.println(distance);
+  Serial.println(" cm");
+  return distance;
+}
+
+void doTurn() {
+  digitalWrite(leftMotor, LOW);
+  delay(turnDelay);
+  turnOnMotor();
+}
+
+void turnOnMotor() {
+  digitalWrite(leftMotor, HIGH);
+  digitalWrite(rightMotor, HIGH);
+}
+
+void turnOffMotor() {
+  digitalWrite(leftMotor, LOW);
+  digitalWrite(rightMotor, LOW);
+}
+
+void doBlink() {
+  digitalWrite(g, HIGH);
+  delay(t);
+  digitalWrite(g, LOW);
+  delay(t);
+}
+
+void setGOn() {
+  digitalWrite(g, HIGH);
+}
+
+void setGOff() {
+  digitalWrite(g, LOW);
+}
+
+void setROn() {
+  digitalWrite(r, HIGH);
+}
+
+void setROff() {
+  digitalWrite(r, LOW);
+}
